980efa30a0bdb82082a346d65e2847c6a1f7c5b8
David Kennedy <kpdmmvi@gmail.com>
2025-08-10 10:07:08 -0400

    Base version of the about me section completed

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -1,10 +1,17 @@
+import portrait from '../assets/portrait.jpg';
 
 function About(props) {
     return (
         <section id="about">
             <h2>About Me</h2>
             <div className="about-content">
-                <img src="..\assets\portrait.jpg" alt="Portrait" width="5rem"></img>
+                <img src={portrait} alt="Portrait" className="portrait"></img>
+                <p>
+                    Second-year Electrical Engineering student with two years of direct experience using CAD programs, 
+                    including SolidWorks and Autodesk Fusion 360, gained through academic and personal projects. 
+                    Eager to expand industry knowledge and technical skills by contributing to real-world projects and collaborating with engineering teams. 
+                    Proven leadership, delegation, and interpersonal communication skills developed through roles as Senior and Assistant Senior Patrol Leader in the Boy Scouts of America. 
+                </p>
             </div>
             
         </section>
