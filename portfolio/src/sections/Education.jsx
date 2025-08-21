f0edad09b2749b6666a75b9255b06140d247d53b
David Kennedy <kpdmmvi@gmail.com>
2025-08-21 01:08:21 -0400

    Completed education section

--- portfolio/src/sections/Education.jsx
+++ portfolio/src/sections/Education.jsx
@@ -1,31 +1,10 @@
 import { useRef, useState, useLayoutEffect } from 'react';
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { education } from "./Details.js";
+import { Schools } from "./Details.js";
 
 function Education() {
     const scrollContentRef = useRef(null);
     const [containerHeight, setContainerHeight] = useState('auto');
-
-    useLayoutEffect(() => {
-        const calculateHeight = () => {
-            if (scrollContentRef.current) {
-                const items = scrollContentRef.current.children;
-                let maxHeight = 0;
-                for (let i = 0; i < items.length; i++) {
-                    if (items[i].offsetHeight > maxHeight) {
-                        maxHeight = items[i].offsetHeight;
-                    }
-                }
-                if (maxHeight > 0) {
-                    setContainerHeight(maxHeight);
-                }
-            }
-        };
-
-        calculateHeight();
-        window.addEventListener('resize', calculateHeight);
-        return () => window.removeEventListener('resize', calculateHeight);
-    }, [skills]);
 
     const scroll = (direction) => {
         if (scrollContentRef.current) {
@@ -38,29 +17,41 @@ function Education() {
         }
     };
 
-    const educationElements = education.map((education, index) => (
-        <div key={index} className="scroll-item">
-            <h3>{education.school}</h3>
-            <h4>{education.degree} {education.gpa}</h4>
-            <p>{education.courses}</p>
+    function mapCourses(courses){
+        return courses.map((course, index) => (
+            <div key={index} >
+                <p style={{textAlign: "left"}}>{course.name} <br /> {course.dates}</p>
+            </div>
+        ));            
+    }
+    
+    const educationElements = Schools.map((school) => (
+        <div 
+            key={school.school} 
+            className="scroll-item"
+        >
+            <h3>{school.school}</h3>
+            <div style={{alignItems: "flex-start"}}>
+                <h5>{school.degree}, GPA {school.gpa}</h5>
+                {mapCourses(school.courses)}
+            </div>
         </div>
     ));
 
     return (
-        <>
-            <h2 style={{marginLeft: "2rem"}}>Skills</h2>
+        <section id="education">
+            <h2 style={{marginLeft: "2rem"}}>Education</h2>
             <div className="scroll-container">
                 <div
                     className="scroll-content"
                     ref={scrollContentRef}
-                    style={{ height: containerHeight === 'auto' ? 'auto' : `${containerHeight}px` }}
                 >
-                    {skillElements}
+                    {educationElements}
                 </div>
                 <button className="scroll-button left" onClick={() => scroll('left')}><FaChevronLeft /></button>
                 <button className="scroll-button right" onClick={() => scroll('right')}><FaChevronRight /></button>
             </div>
-        </>
+        </section>
     )
 }
 export default Education;
