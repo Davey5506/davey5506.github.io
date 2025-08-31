65f0d5e27c226d33c015c954fa1656ddc14d415f
Davey5506 <kpdmmvi@gmail.com>
2025-08-31 15:48:21 -0400

    Adjusted style

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -5,12 +5,17 @@ function About(props) {
         <div className="about">
             <h2>About Me</h2>
             <div className="about-content">
-                <img src={portrait} alt="Portrait" className="portrait"></img>
+                <div className="portrait-container">
+                    <img src={portrait} alt="Portrait" className="portrait"></img>
+                    <h4 style={{textAlign: "center"}}>David Kennedy</h4>
+                </div>
                 <p>
-                    Second-year Electrical Engineering student with two years of direct experience using CAD programs, 
-                    including SolidWorks and Autodesk Fusion 360, gained through academic and personal projects. 
-                    Eager to expand industry knowledge and technical skills by contributing to real-world projects and collaborating with engineering teams. 
-                    Proven leadership, delegation, and interpersonal communication skills developed through roles as Senior and Assistant Senior Patrol Leader in the Boy Scouts of America. 
+                    I am a second-year Electrical Engineering student with two years of hands-on experience with CAD programs, 
+                    such as SolidWorks and Autodesk Fusion 360 gotten through school and personal projects. 
+                    Eager to enhance my industry knowledge and technical skills 
+                    through contributing to real-world projects and collaborating with a team of engineers. 
+                    I have developed skills in team leadership, delegation, and interpersonal communication 
+                    through Boy Scouts of America while working towards my Eagle Scout.
                 </p>
             </div>
             
